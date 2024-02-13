ac4322ccd90144bdf3fc552942bb407a4445e99d
Piotr Wrobel <wrobelpiotr24@gmail.com>
2024-02-13 16:49:26 +0100

    sign in page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Label, TextInput, Button, Toast, Alert, Spinner } from 'flowbite-react'
 
 const SignIn = () => {
-    return (
-        <div>
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [loading, SetLoading] = useState(false)
+    const navigate = useNavigate();
 
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.email || !formData.password) {
+            return setErrorMessage('Please fill out all fields.');
+        }
+        console.log("jestem");
+        try {
+            setErrorMessage(null);
+            SetLoading(true)
+            const res = await fetch("/api/auth/signin", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+            SetLoading(false)
+            if (data.success === false) {
+                setErrorMessage(data.message)
+            }
+
+            if (res.ok) {
+                navigate("/")
+            }
+
+        } catch (error) {
+            setErrorMessage(error.message)
+            SetLoading(false)
+        }
+    }
+
+
+    return (
+        <div className='min-h-screen mt-20 '>
+            <div className='flex p-3 gap-10 max-w-4xl mx-auto flex-col md:flex-row md:items-center px-6'>
+                {/* Left */}
+                <div className="flex flex-1 flex-col">
+                    <Link
+                        to={"/"}
+                        className='text-4xl font-bold dark:text-white'>
+                        <span className='px-2 py-1 text-white rounded-lg bg-gradient-to-r from-emerald-400 to-emerald-200'>Sahand's</span>Blog
+                    </Link>
+                    <p className='text-sm mt-5'>
+                        This is a demo project. You can sign in with your email and password
+                        or with Google.
+                    </p>
+                </div>
+                {/* right */}
+                <div className="flex-1 ">
+                    <form className='flex flex-col gap-4 text-xl' onSubmit={handleSubmit}>
+                        <div>
+                            <Label value='Your email'></Label>
+                            <TextInput
+                                type='email'
+                                placeholder='name@company.com'
+                                id='email'
+                                onChange={handleChange}>
+                            </TextInput>
+                        </div>
+                        <div>
+                            <Label value='Your password'></Label>
+                            <TextInput
+                                type='password'
+                                placeholder='***************'
+                                id='password'
+                                onChange={handleChange}>
+                            </TextInput>
+                        </div>
+                        <Button
+                            className='text-white mt-4 font-bold bg-gradient-to-r from-emerald-400 to-emerald-200 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-emerald-400 dark:focus:ring-green-800 px-2 py-1 text-center mb-2'
+                            disabled={loading}
+                            type='submit'>
+                            {loading
+                                ? <><Spinner
+                                    size="sm"
+                                    color={"emerald"}
+                                    className='mr-4' />
+                                    <span>Loading...</span></>
+                                : "Sign In"
+                            }
+                        </Button>
+                    </form>
+                    <div className='flex gap-3 text-sm'>
+                        <span >You don't have account yet?</span>
+                        <Link
+                            to={"/sign-up"}
+                            className='text-md font-bold text-emerald-600 dark:text-white'>
+                            Sign Up
+                        </Link>
+                    </div>
+                    {errorMessage &&
+                        <Alert color="failure" className='mt-5'>
+                            {errorMessage}
+                        </Alert>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
