a042e8fa9f81993e168c085386592efd9e39506d
Piotr Wrobel <wrobelpiotr24@gmail.com>
2024-02-13 16:36:03 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "./utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
 
+dotenv.config()
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
@@ -25,6 +28,37 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-
-
 };
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password || email === "" || password === "") {
+        return next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return next(errorHandler(404, "Something goes wrong"))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return next(errorHandler(400, "Something goes wrong"))
+        }
+        validUser.password = null
+
+        const token = jwt.sign(
+            { userId: validUser._id },
+            process.env.JWT_SECRET_KEY
+        )
+
+        res
+            .status(200)
+            .cookie("access_token", token, { httpOnly: true })
+            .json(validUser)
+
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/signin.route.js
+++ api/routes/signin.route.js
@@ -1,8 +1,7 @@
 import express from "express"
 const router = express.Router();
-import { signup, signIn } from "../controllers/auth.controller.js"
+import { signup } from "../controllers/auth.controller.js"
 
 router.post("/signup", signup)
-router.post("/signin", signIn)
 
 export default router
